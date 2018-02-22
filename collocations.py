2081e41f295804fb485703fabae963a13e207571
Kavi Mehta <kavi.mehta2@gmail.com>
2018-02-22 13:48:47 -0800

    python 3 compatible print statements

--- collocations.py
+++ collocations.py
@@ -9,10 +9,10 @@ finder = BigramCollocationFinder.from_words(
 ignored_words = nltk.corpus.stopwords.words('english')
 finder.apply_word_filter(lambda w: len(w) < 3 or w.lower() in ignored_words)
 finder.apply_freq_filter(5)
-print finder.nbest(bigram_measures.likelihood_ratio, 20)
+print(finder.nbest(bigram_measures.likelihood_ratio, 20))
 finder = TrigramCollocationFinder.from_words(
          tokens, 5)
 ignored_words = nltk.corpus.stopwords.words('english')
 finder.apply_word_filter(lambda w: len(w) < 3 or w.lower() in ignored_words)
 finder.apply_freq_filter(5)
-print finder.nbest(trigram_measures.likelihood_ratio, 20)
+print(finder.nbest(trigram_measures.likelihood_ratio, 20))
