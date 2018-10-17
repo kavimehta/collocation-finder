c627c074fcb30f4449be8b201284c2fd41a26660
Kavi Mehta <kavi.mehta2@gmail.com>
2018-10-17 11:31:45 -0700

    Fixed Corpus, S1Bigram implementation file syntax

--- Corpus.cpp
+++ Corpus.cpp
@@ -2,188 +2,180 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "Corpus.h"
 using namespace std;
 
 /**
  * This class represents the text file that makes up a corpus.
  */
-class Corpus {
-
-private:
-    ifstream file;
-
-public:
-
-	string filename;
     
-    /**
-     * Constructor for the Corpus.
-     *
-     * @param filename_ The text file that contains the corpus.
-     */
-	Corpus(string filename_) {
-		filename = filename_;
-	}
+/**
+ * Constructor for the Corpus.
+ *
+ * @param filename_ The text file that contains the corpus.
+ */
+Corpus::Corpus(string filename_) {
+    filename = filename_;
+}
 
-    /**
-     * Count all the lines in the corpus and print to cout.
-     */
-	void countLines() {
-        //Open the file
-        ifstream file;
-		file.open(filename);
+/**
+ * Count all the lines in the corpus and print to cout.
+ */
+void Corpus::countLines() {
+    //Open the file
+    ifstream file;
+    file.open(filename);
 
-		int count = 0;
-		string line;
-		while (getline(file, line)) {
-			count++;
-		}
-		cout << "Lines: " << count;
-
-        //Close the file
-        file.close();
-	}
-
-    /**
-     * This returns a Vector of Strings, where each String is a sentence in the
-     * Corpus that contains the specified word.
-     *
-     * @param word_ The word that is searched for in the corpus.
-     * @return The Vector of Strings with the sentences.
-     */
-	vector<string> getSentencesWith(string word_) {
-        //Open the file
-        ifstream file;
-		file.open(filename);
-
-		vector<string> foundSentences;
-		string record;
-		while (getline(file, record)) {
-			// Add spaces so record.find catches word at beginning or end
-			string tempRecord = " " + record + " ";
-			if (tempRecord.find(" " + word_ + " ")) {
-				foundSentences.push_back(record);
-			}
-		}
-		
-        //Close the file
-        file.close();
-
-		return foundSentences;
-	}
-
-	/**
-     * This returns a Vector of Strings, where each String is a sentence in the
-     * Corpus that contains the specified words, with w2 being
-     * distance words away from w1.
-     *
-     * @param w1       The first word
-     * @param w2       The second word
-     * @param distance The distance between them. -5 to -1 and 1 to 5 are valid values.
-     * @return A Vector of Strings with one matched sentence per string.
-     */
-    vector<string> getSentencesWith(string w1, string w2, int distance) {
-        //Open the file
-        ifstream file;
-		file.open(filename);
-
-        vector<string> foundSentences;
-        string record;
-        while (getline(file, record)) {
-            // Remove punctuation
-            for (int i = 0, len = record.size(); i < len; i++) {
-                    if (ispunct(record[i])) {
-                            record.erase(i--, 1);
-                            len = record.size();
-                    }
-                }
-
-            // Split string into words
-            istringstream iss(record);
-            vector<string> words;
-                copy(istream_iterator<string>(iss), istream_iterator<string>(), back_inserter(words));
-        
-            // Find w1 and check if it is the proper distance from w2
-                bool addS = false;
-                for (int i = 0; i < words.size(); i++) {
-                    if (words[i] == w1) {
-                            if (i + distance >=0 && i+distance < words.size()) {
-                                if (words[i+distance] == w2) {
-                                    addS = true;
-                                    break;
-                                }
-                            }
-                    }
-                }
-
-                // If found a match, add the sentence to the vector
-                if (addS) {
-                    foundSentences.push_back(record);
-                }
-        }
-        
-        //Close the file
-        file.close();
-
-        return foundSentences;
+    int count = 0;
+    string line;
+    while (getline(file, line)) {
+        count++;
     }
+    cout << "Lines: " << count;
 
-    vector<string> getFrequentWords(int minFrequency) {
-        //Open the file
-        ifstream file;
-		file.open(filename);
+    //Close the file
+    file.close();
+}
 
-        vector<string> freqWords;
-        // Create a map
-        map<string, int> m;
+/**
+ * This returns a Vector of Strings, where each String is a sentence in the
+ * Corpus that contains the specified word.
+ *
+ * @param word_ The word that is searched for in the corpus.
+ * @return The Vector of Strings with the sentences.
+ */
+vector<string> Corpus::getSentencesWith(string word_) {
+    //Open the file
+    ifstream file;
+    file.open(filename);
 
-        bool isStopWord = false;
-        const char* args[] = {"was", "am", "has", "the", "a", "and", "be", "but", "by", "can", "such", "could", "do", "for", "have", "him", "her", "i", "is", "we", "he", "she", "it", "may", "might", "mine", "must", "need", "no", "not", "nor", "none", "our", "where", "whether", "while", "which", "you", "your", "to", "of", "on", "with", "in", "so", "or", "my", "its", "if", "his", "hers", "as", "an", "at", "this", "they", "there", "then", "that", "are", "would", "who", "whom", "them", "each", "from", "ourselves", "when", "these"};
-        vector<string> stopWords(args, args + sizeof(args)/sizeof(args[0]));
+    vector<string> foundSentences;
+    string record;
+    while (getline(file, record)) {
+        // Add spaces so record.find catches word at beginning or end
+        string tempRecord = " " + record + " ";
+        if (tempRecord.find(" " + word_ + " ")) {
+            foundSentences.push_back(record);
+        }
+    }
+    
+    //Close the file
+    file.close();
 
-        string record;
-        while (getline(file, record)) {
-            // Remove punctuation
-            for (int i = 0, len = record.size(); i < len; i++) {
+    return foundSentences;
+}
+
+/**
+ * This returns a Vector of Strings, where each String is a sentence in the
+ * Corpus that contains the specified words, with w2 being
+ * distance words away from w1.
+ *
+ * @param w1       The first word
+ * @param w2       The second word
+ * @param distance The distance between them. -5 to -1 and 1 to 5 are valid values.
+ * @return A Vector of Strings with one matched sentence per string.
+ */
+vector<string> Corpus::getSentencesWith(string w1, string w2, int distance) {
+    //Open the file
+    ifstream file;
+    file.open(filename);
+
+    vector<string> foundSentences;
+    string record;
+    while (getline(file, record)) {
+        // Remove punctuation
+        for (int i = 0, len = record.size(); i < len; i++) {
                 if (ispunct(record[i])) {
                         record.erase(i--, 1);
                         len = record.size();
                 }
             }
 
-            // Split string into words
-            istringstream iss(record);
-            vector<string> words;
+        // Split string into words
+        istringstream iss(record);
+        vector<string> words;
             copy(istream_iterator<string>(iss), istream_iterator<string>(), back_inserter(words));
-
+    
+        // Find w1 and check if it is the proper distance from w2
+            bool addS = false;
             for (int i = 0; i < words.size(); i++) {
-                for (int j = 0; j < stopWords.size(); j++) {
-                        if (words[i] == words[j]) {
-                            isStopWord = true;
-                            break;
+                if (words[i] == w1) {
+                        if (i + distance >=0 && i+distance < words.size()) {
+                            if (words[i+distance] == w2) {
+                                addS = true;
+                                break;
+                            }
                         }
                 }
-
-                if (!isStopWord) {
-                        m[words[i]]++;
-                }
-
-                isStopWord = false;
             }
 
-            // Add frequent words to freqWords
-            map<string, int>::iterator it;
-            for (it = m.begin(); it != m.end(); it++) {
-                if (it->second >= minFrequency) {
-                        freqWords.push_back(it->first);
-                }
+            // If found a match, add the sentence to the vector
+            if (addS) {
+                foundSentences.push_back(record);
+            }
+    }
+    
+    //Close the file
+    file.close();
+
+    return foundSentences;
+}
+
+vector<string> Corpus::getFrequentWords(int minFrequency) {
+    //Open the file
+    ifstream file;
+    file.open(filename);
+
+    vector<string> freqWords;
+    // Create a map
+    map<string, int> m;
+
+    bool isStopWord = false;
+    const char* args[] = {"was", "am", "has", "the", "a", "and", "be", "but", "by", "can", "such", "could", "do", "for", "have", "him", "her", "i", "is", "we", "he", "she", "it", "may", "might", "mine", "must", "need", "no", "not", "nor", "none", "our", "where", "whether", "while", "which", "you", "your", "to", "of", "on", "with", "in", "so", "or", "my", "its", "if", "his", "hers", "as", "an", "at", "this", "they", "there", "then", "that", "are", "would", "who", "whom", "them", "each", "from", "ourselves", "when", "these"};
+    vector<string> stopWords(args, args + sizeof(args)/sizeof(args[0]));
+
+    string record;
+    while (getline(file, record)) {
+        // Remove punctuation
+        for (int i = 0, len = record.size(); i < len; i++) {
+            if (ispunct(record[i])) {
+                    record.erase(i--, 1);
+                    len = record.size();
             }
         }
 
-        //Close the file
-        file.close();
+        // Split string into words
+        istringstream iss(record);
+        vector<string> words;
+        copy(istream_iterator<string>(iss), istream_iterator<string>(), back_inserter(words));
 
-        return freqWords;
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = 0; j < stopWords.size(); j++) {
+                    if (words[i] == words[j]) {
+                        isStopWord = true;
+                        break;
+                    }
+            }
 
+            if (!isStopWord) {
+                    m[words[i]]++;
+            }
+
+            isStopWord = false;
+        }
+
+        // Add frequent words to freqWords
+        map<string, int>::iterator it;
+        for (it = m.begin(); it != m.end(); it++) {
+            if (it->second >= minFrequency) {
+                    freqWords.push_back(it->first);
+            }
+        }
     }
-};
+
+    //Close the file
+    file.close();
+
+    return freqWords;
+
+}
--- S1Bigram.cpp
+++ S1Bigram.cpp
@@ -1,44 +1,35 @@
 #include <bits/stdc++.h>
+#include "S1Bigram.h"
 using namespace std;
 
 /**
  * A small class to hold a bigram and the results of Stage 1.
  */
-class S1Bigram {
-public:
- 
-    string w;
-    string wi;
-    double strength;
-    double spread;
-    vector<int> distances;
 
-    S1Bigram(string w_, string wi_, double strength_, double spread_, std::vector<int> distances_) {
-        w = w_;
-        wi = wi_;
-        strength = strength_;
-        spread = spread_;
-        distances = distances_;
-    }
+S1Bigram::S1Bigram(string w_, string wi_, double strength_, double spread_, std::vector<int> distances_) {
+    w = w_;
+    wi = wi_;
+    strength = strength_;
+    spread = spread_;
+    distances = distances_;
+}
 
-    string getw() {
-    	return w;
-    }
+string S1Bigram::getw() {
+    return w;
+}
 
-    string getwi() {
-    	return wi;
-    }
+string S1Bigram::getwi() {
+    return wi;
+}
 
-    double getStrength() {
-    	return strength;
-    }
+double S1Bigram::getStrength() {
+    return strength;
+}
 
-    double getSpread() {
-    	return spread;
-    }
+double S1Bigram::getSpread() {
+    return spread;
+}
 
-    std::vector<int> getDistances() {
-    	return distances;
-    }
-    
-};
+std::vector<int> S1Bigram::getDistances() {
+    return distances;
+}
